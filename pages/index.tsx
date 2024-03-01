3c3b516ac9dd93ff05bdc64d6ea08dc5e409a926
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2024-03-01 16:41:03 +0530

    css added;implemnts issue #4

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <h1 style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20px" }}>
-        E-Commerce Data
-      </h1>
+      <h1 className="flex bg-slate-400 justify-center">E-Commerce Data</h1>
       <HomePage />
     </>
   );
